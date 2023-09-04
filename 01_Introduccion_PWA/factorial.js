d04c4176aa0a1f83879da088947c6e69a1adf07f
Fernando Sánchez Noguez <fer_noguez890@outlook.com>
2023-09-04 17:43:30 -0600

    factorial.js modificado con un callback

--- 01_Introduccion_PWA/factorial.js
+++ 01_Introduccion_PWA/factorial.js
@@ -4,6 +4,16 @@ const factorial = (n) => {
     for (let i = 1; i <= n ; i++){
         acum *= i; 
     }
-    
+
     return acum;
 }
+
+const suma = (sumar) => {
+    sumar(2,3)
+}
+
+function sumar(n1, n2){
+    console.log(n1 + n2);
+}
+
+suma(sumar)
